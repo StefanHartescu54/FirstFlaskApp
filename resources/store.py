ba9adc65c7f62788f22b366acfab3dd0f9326f37
StefanHartescu54 <stefan.hartescu10@gmail.com>
2025-11-06 15:39:28 +0200

    switch to using sqllite

--- resources/store.py
+++ resources/store.py
@@ -3,42 +3,47 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from schemas import StoreSchema
+from models import StoreModel
+from db import db
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 blp = Blueprint("Stores", "stores", description="Operations on stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
     @blp.response(200,StoreSchema)
-    def get(cls, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found")
+    def get(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+        return store
+
 
     def delete(cls, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "store deleted"}
-        except KeyError:
-            abort(404, message="Store not found")
-
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message" : "Store deleted"}
+    
 @blp.route("/store")
 class StoreList(MethodView):
     @blp.response(200,StoreSchema(many=True))
-    def get(cls):
-        return stores.values()
+    def get(self):
+        return StoreModel.query.all()
 
     @blp.arguments(StoreSchema)
     @blp.response(201,StoreSchema)
-    def post(cls, store_data):  # store_data is already validated and parsed
-        # Check for existing store
-        for store in stores.values():
-            if store.get("name") == store_data["name"]:
-                abort(400, message=f"Store already exists.")
-
-        # Create new store
-        store_id = uuid.uuid4().hex
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
-        
+    def post(self, store_data):  # store_data is already validated and parsed
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(
+                400,
+                message= "A store with that name already exists.",
+            )
+        except SQLAlchemyError:
+            abort(
+                500,
+                message = "An error occured creating the store."
+            )
         return store
